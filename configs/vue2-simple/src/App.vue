8e0bf4809cf4f39287f8747a8c87a50697413e3b
Strixis <nivem-strixis@mail.ru>
2021-03-01 19:15:47 +0300

    Протестирован конфиг для vue2-simple

---
+++ configs/vue2-simple/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <Test />
+  </div>
+</template>
+
+<script>
+import Test from 'components/Test';
+
+export default {
+  components: {
+    Test
+  },
+}
+</script>
+
+<style lang="sass">
+@use 'styles'
+</style>
---
+++ configs/vue2-simple/src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img :src="logo" alt="logo">
+    <p class="test">Hello!</p>
+  </div>
+</template>
+
+<script>
+import logo from 'img/logo.svg';
+
+export default {
+  data() {
+    return {
+      logo,
+    }
+  }
+}
+</script>
