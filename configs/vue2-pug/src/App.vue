eefeb9c7bce2a2ecdc00c9ec583b0751083327a2
Strixis <nivem-strixis@mail.ru>
2021-03-13 14:04:51 +0300

    Добавлен и протестирован конфиг для vue2-pug

---
+++ configs/vue2-pug/src/App.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .container
+    Test
+    include templates/test
+</template>
+
+<script>
+import Test from 'components/Test';
+
+export default {
+  components: {
+    Test
+  },
+}
+</script>
+
+<style lang="sass">
+@use 'styles'
+</style>
---
+++ configs/vue2-pug/src/components/Test.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    include ../templates/test
+    img(:src="logo" alt="logo")
+    p.test Hello!
+</template>
+
+<script>
+import logo from 'img/logo.svg';
+
+export default {
+  data() {
+    return {
+      logo,
+    }
+  }
+}
+</script>
